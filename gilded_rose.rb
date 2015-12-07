8f59daadc56ef22564e17f2d4254121cb7f320c0
Tom von Schwerdtner <tvon@covermymeds.com>
2015-12-07 12:08:57 -0500

    Reduce changes from original kata
    Remove tests and any instructions beyond the README.

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/env ruby
+
 def update_quality(items)
   items.each do |item|
     if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
@@ -46,18 +48,37 @@ def update_quality(items)
   end
 end
 
-# DO NOT CHANGE THINGS BELOW -----------------------------------------
+class Item
+  attr_accessor :name, :sell_in, :quality
 
-Item = Struct.new(:name, :sell_in, :quality)
+  def initialize(name, sell_in, quality)
+    @name = name
+    @sell_in = sell_in
+    @quality = quality
+  end
 
-# We use the setup in the spec rather than the following for testing.
-#
-# Items = [
-#   Item.new("+5 Dexterity Vest", 10, 20),
-#   Item.new("Aged Brie", 2, 0),
-#   Item.new("Elixir of the Mongoose", 5, 7),
-#   Item.new("Sulfuras, Hand of Ragnaros", 0, 80),
-#   Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-#   Item.new("Conjured Mana Cake", 3, 6),
-# ]
+  def to_s
+    sprintf("% 50s %10d %10d", name, sell_in, quality)
+  end
+end
 
+items = [
+  Item.new("+5 Dexterity Vest", 10, 20),
+  Item.new("Aged Brie", 2, 0),
+  Item.new("Elixir of the Mongoose", 5, 7),
+  Item.new("Sulfuras, Hand of Ragnaros", 0, 80),
+  Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+  Item.new("Conjured Mana Cake", 3, 6),
+]
+
+puts ""
+puts sprintf("% 50s %10s %10s", "Name", "Sell In", "Quality")
+puts "========================================================================"
+items.each { |i| puts i }
+
+update_quality(items)
+
+puts ""
+puts sprintf("% 50s %10s %10s", "Name", "Sell In", "Quality")
+puts "========================================================================"
+items.each { |i| puts i }
